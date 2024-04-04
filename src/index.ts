4197083e594b6ea765bb94feafa7c76de2114bc2
Antti Kleemola <kleeant@gmail.com>
2024-04-04 12:27:52 +0300

    azure test without db

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import config from './infrastructure/config/config.dotenv'
 import databaseConnection from './infrastructure/database/database.connection'
 import loggerService from './lib/logger'
 const run = async (): Promise<void> => {
-  await databaseConnection.ensureLatestDatabase()
+  // await databaseConnection.ensureLatestDatabase()
   const app = server()
   app.listen(config.API_PORT, () => {
     loggerService.info(`API listening on port ${config.API_PORT}`)
