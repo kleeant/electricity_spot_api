dde7895f6196c41fb9a4e21139b83489c341562d
Antti Kleemola <kleeant@gmail.com>
2024-04-04 14:14:33 +0300

    feat(azure): more error logging

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import config from './infrastructure/config/config.dotenv'
 import databaseConnection from './infrastructure/database/database.connection'
 import loggerService from './lib/logger'
 const run = async (): Promise<void> => {
-  // await databaseConnection.ensureLatestDatabase()
-  const app = server()
-  app.listen(config.API_PORT, () => {
-    loggerService.info(`API listening on port ${config.API_PORT}`)
-    loggerService.info(`API docs at http://localhost:${config.API_PORT}/openapi/ui/`)
-  })
+  try {
+    await databaseConnection.ensureLatestDatabase()
+    const app = server()
+    app.listen(config.API_PORT, () => {
+      loggerService.info(`API listening on port ${config.API_PORT}`)
+      loggerService.info(`API docs at http://localhost:${config.API_PORT}/openapi/ui/`)
+    })
+  }catch(e) {
+    loggerService.error(e as Error)
+  }
+  
 }
 
 void run()
